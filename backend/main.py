cf24bc98e15a78fadf4924e3ac4712e1e5dd2127
ganeshdevnale16 <ganesh.devnale16@gmail.com>
2026-03-22 18:26:07 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -825,7 +825,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+    "https://compliancemonitoring.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
