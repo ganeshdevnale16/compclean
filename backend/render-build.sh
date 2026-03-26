dd583271ba994ff823382ab1bb77e05476de7f2f
ganeshdevnale16 <ganesh.devnale16@gmail.com>
2026-03-26 13:13:22 +0530

    Update render-build.sh

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,8 +1,31 @@
+# #!/usr/bin/env bash
+
+# echo "Installing Chromium..."
+
+# apt-get update || true
+# apt-get install -y chromium chromium-driver || true
+
+# echo "Done"
+
+
+
+
+
+
+
 #!/usr/bin/env bash
 
-echo "Installing Chromium..."
+echo "Updating packages..."
 
-apt-get update || true
-apt-get install -y chromium chromium-driver || true
+apt-get update -y
+
+echo "Installing Chromium + Driver..."
+
+apt-get install -y chromium chromium-driver
+
+echo "Checking installation..."
+
+which chromium
+which chromedriver
 
 echo "Done"
